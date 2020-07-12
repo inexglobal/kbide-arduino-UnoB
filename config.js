4a97bb560243b699403dc739e7191d0867d8f711
inexglobal <37691139+inexglobal@users.noreply.github.com>
2020-07-12 20:49:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: "arduino-UnoB",
   platform: "arduino-avr",
-  title: "i-Duino R3B.",
+  title: "i-Duino R3B",
   description: "i-Duino R3B - Beginer board for learning Arduino",
   author: "INEX",
   website: "https://inex.co.th/shop/education-board/arduino-wiring/i-duino-uno/i-duino-uno-r3b.html",
   email: "teerawut@inex.co.th",
   git: "https://github.com/inexglobal/kbide-arduino-UnoB/",
   image: "/static/display.jpg",
-  version: "0.9.3"
+  version: "0.9.2"
 };
